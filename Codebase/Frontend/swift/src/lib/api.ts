57f19b0addfd3599d0058f87d44a457c57bb4fe5
Shokudaikiri <solomonkaniaru154@gmail.com>
2025-03-11 15:44:01 +0300

    Update: deployment

--- Codebase/Frontend/swift/src/lib/api.ts
+++ Codebase/Frontend/swift/src/lib/api.ts
@@ -1,10 +1,8 @@
 import axios from 'axios';
 import { Product } from './data';
-import dotenv from 'dotenv';
-dotenv.config();
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000/api',
+  baseURL: import.meta.env.VITE_API_URL
 });
 
 // Add auth token to requests
